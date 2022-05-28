35c835472aaef877821348387e3dff538ec41740
Caleb SMith <calebsmithwoolrich@gmail.com>
2022-05-28 15:38:00 -0400

    exp

--- src/src-v2-rust-alpha/src/aog/lcd.rs
+++ src/src-v2-rust-alpha/src/aog/lcd.rs
@@ -3,65 +3,66 @@ use std::thread;
 use std::time::Duration;
 extern crate machine_ip;
 
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+
 pub fn init(){
 
-    thread::spawn(|| {
+    thread::spawn(|| loop {
 
         // Default LCDSize is 4x20
         let mut config = ScreenConfig::default();
-
-        // Uncomment and modify the values below to use different screen sizes
-        config.max_rows = 4;
-        config.max_columns = 20;
     
         // Default Qwiic address is 0x72
         let mut screen = Screen::new(config, "/dev/i2c-1", 0x72).expect("Could not init device");
     
-        // Set backlight to green and wait 1 second
-        screen.change_backlight(0, 255, 0).unwrap();
-        thread::sleep(Duration::from_secs(1));
-    
-        // Set backlight to bright white
-        screen.change_backlight(255, 255, 255).unwrap();
 
-        loop {
-            // Clear the screen
-            screen.clear().unwrap();
-            
-            // Move the cursor to 0,0
-            screen.move_cursor(0,0).unwrap();
+        let default = set_lcd(screen);
 
-            // Fetch IP Address
-            let ip = machine_ip::get().unwrap();
-        
-            // Print text
-            let p1 = screen.print(format!("{}", ip.to_string()).as_str()).unwrap();
-        
-            // Move to the next line
-            screen.move_cursor(1,0).unwrap();
-        
-            // Print text
-            screen.print(format!("CO2: {}ppm", 0).as_str()).unwrap();
-
-            // Move to the next line
-            screen.move_cursor(2,0).unwrap();
-
-            // Print text
-            screen.print(format!("PM2.5: {}", 0).as_str()).unwrap();
-
-            // Move to the next line
-            screen.move_cursor(3,0).unwrap();
-
-            // Print text
-            screen.print(format!("Status: 001").as_str()).unwrap();
-
-            // Sleep
-            thread::sleep(Duration::from_millis(500));
-        }
-    
-
+        thread::sleep(Duration::from_secs(2));
 
     });
 
 }
 
+pub fn set_lcd(mut screen: Screen) -> Result<Screen, LinuxI2CError>{
+    // Set backlight to green and wait 1 second
+    screen.change_backlight(0, 255, 0)?;
+    thread::sleep(Duration::from_secs(1));
+
+    // Set backlight to bright white
+    screen.change_backlight(255, 255, 255)?;
+
+    // Clear the screen
+    screen.clear()?;
+        
+    // Move the cursor to 0,0
+    screen.move_cursor(0,0)?;
+
+
+    // Fetch IP Address
+    let ip = machine_ip::get().unwrap();
+
+    // Print text
+    let p1 = screen.print(format!("{}", ip.to_string()).as_str())?;
+
+    // Move to the next line
+    screen.move_cursor(1,0)?;
+
+    // Print text
+    screen.print(format!("CO2: {}ppm", 0).as_str())?;
+
+    // Move to the next line
+    screen.move_cursor(2,0)?;
+
+    // Print text
+    screen.print(format!("PM2.5: {}", 0).as_str())?;
+
+    // Move to the next line
+    screen.move_cursor(3,0)?;
+
+    // Print text
+    screen.print(format!("Status: 001").as_str())?;
+
+
+    return Ok(screen);
+}
